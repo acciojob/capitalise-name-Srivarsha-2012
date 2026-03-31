d7bdfa3fc08b0c71df46379f1c662bce4b821419
srivarsha <algotesrivarsha@gmail.com>
2026-03-31 22:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function convertToUppercase() {
+  const input = document.getElementById("fname");
+  input.value = input.value.toUpperCase();
+}
+
+// Attach event when input loses focus
+document.getElementById("fname").addEventListener("blur", convertToUppercase);
